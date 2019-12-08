c2e40220534d7404f2928ee44bc2b4a9b0f89865
k-jasinska <jasinskak64@gmail.com>
2019-12-08 21:33:37 +0100

    dodanie fragmentu i przyciskow

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,10 +1,22 @@
 package com.example.myapplication;
 
+import androidx.fragment.app.Fragment;
+
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TimePicker;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +24,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addBtn = (Button)findViewById(R.id.add);
+        final Fragment f2=new Form();
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.frameLayout, f2)
+                        .commit();
+            }
+        });
+
+
         Button btn1 = findViewById(R.id.menu_left);
+
         btn1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, NotatkaActivity.class);
