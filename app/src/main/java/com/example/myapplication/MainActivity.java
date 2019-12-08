a80b70a0644c0eb66c261cee0482f8321cc3690b
woolenrainbow <janusadr@gmail.com>
2019-12-08 22:17:22 +0100

    Dodanie nawigacji do listy.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.myapplication;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
         btn1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, NotatkaActivity.class);
+                startActivity(i);
+            }
+        });
+        Button btn2 = findViewById(R.id.menu_right);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ListaActivity.class);
                 startActivity(i);
             }
         });
